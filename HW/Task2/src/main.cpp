2ccd658adfd57077603f92beee8798e33fa90d7f
OG_Michael <59878471+M1SH4-cmd@users.noreply.github.com>
2025-01-25 01:49:20 +0700

    Task2 reworked

--- HW/Task2/src/main.cpp
+++ HW/Task2/src/main.cpp
@@ -6,18 +6,21 @@ using namespace std;
 
 int main()
 {
-    map<int, string> people;
+    map<string, int> people;
     cout << "Enter names (type 'Next' to see the next person, 'exit' to quit):" << endl;
-
     while(true) {
         string person;
         cin >> person;
-        people.insert(make_pair(people.size() + 1, person));
+        people.insert(make_pair(person, people.size() + 1));
         if (person == "Next") {
-            cout << people.begin()->second << endl;
+            if(people.empty()) {
+                cout << "No more people to display." << endl;
+                continue;
+            }
+            cout << people.begin()->first << endl;
+            people.erase(people.begin()->first);
         }
         if(person == "exit") return 0;
     }
-
 
 }
